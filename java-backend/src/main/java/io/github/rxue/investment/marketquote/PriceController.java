b1be446ca963de0c4ae2e16f50d66410f46bec8f
Rui Xue <ruixue.fi@gmail.com>
2026-06-15 17:55:53 +0300

    #43 fetcher updated

--- java-backend/src/main/java/io/github/rxue/investment/marketquote/PriceController.java
+++ java-backend/src/main/java/io/github/rxue/investment/marketquote/PriceController.java
@@ -1,5 +1,10 @@
 package io.github.rxue.investment.marketquote;
 
+import org.springframework.data.util.Pair;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +18,13 @@ public class PriceController {
         this.yahooFinanceFetcher = yahooFinanceFetcher;
     }
     @GetMapping("/price/{companySymbol}")
-    public Price getPrice(@PathVariable String companySymbol) throws IOException {
-        return yahooFinanceFetcher.getCurrentPrice(companySymbol);
+    public ResponseEntity<Price> getPrice(@PathVariable String companySymbol, @CookieValue(required = false) String crumb) throws IOException {
+        Pair<String, Price> crumbAndPrice = yahooFinanceFetcher.getCurrentPrice(companySymbol);
+        ResponseCookie crumbCookie = ResponseCookie.from("crumb", crumbAndPrice.getFirst())
+                .path("/")
+                .build();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, crumbCookie.toString())
+                .body(crumbAndPrice.getSecond());
     }
 }
